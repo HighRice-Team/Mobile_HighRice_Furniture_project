ba1512c27140264fc603b645d570f1e1ec3568a2
jinssu <jinssu6489@naver.com>
2018-02-24 21:34:04 +0900

    update R_Chart

--- STS_HighRice_NewTable/src/main/java/com/bit_fr/db/MemberManager.java
+++ STS_HighRice_NewTable/src/main/java/com/bit_fr/db/MemberManager.java
@@ -13,7 +13,6 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import com.bit_fr.vo.MemberVo;
 import com.bit_fr.vo.OrderlistVo;
 
-
 public class MemberManager {
 	private static SqlSessionFactory factory;
 	static {
@@ -111,15 +110,16 @@ public class MemberManager {
 
 		return re;
 	}
-	//매니저 분류
+
+	// 매니저 분류
 	public static int getGrade_member(String member_id) {
 		SqlSession session = factory.openSession();
 		HashMap<String, String> map = new HashMap<String, String>();
 		map.put("member_id", member_id);
 		int re = session.selectOne("member.getGrade_member", map);
-		
+
 		session.close();
-		
+
 		return re;
 	}
 
@@ -193,5 +193,13 @@ public class MemberManager {
 	}
 
 	// Delete
+
+	public static List<String> getAllJumin_member() {
+		SqlSession session = factory.openSession();
+		List<String> list = session.selectList("member.getAllJumin_member");
+		session.close();
+
+		return list;
+	}
 
 }
