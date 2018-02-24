36305376bbc5824f350212d1d7b753065f641626
SW <SW@LAPTOP-OP0UKF15>
2018-02-24 13:58:53 +0900

    hjhgj

--- STS_HighRice_NewTable/src/main/java/com/bit_fr/filter/LoginFilter.java
+++ STS_HighRice_NewTable/src/main/java/com/bit_fr/filter/LoginFilter.java
@@ -15,51 +15,50 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class LoginFilter implements Filter {
 
-	/**
-	 * Default constructor.
-	 */
-	public LoginFilter() {
-	}
+   /**
+    * Default constructor.
+    */
+   public LoginFilter() {
+   }
 
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-	}
+   /**
+    * @see Filter#destroy()
+    */
+   public void destroy() {
+   }
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
-		String id = null;
-		
-		String gotoPage="";
-		if(gotoPage.equals("")||gotoPage.equals(null)) {
-			gotoPage="";
-		}else {
-			gotoPage = request.getParameter("gotoPage");
-		}
-		
-		if (((HttpServletRequest) request).getSession().getAttribute("id") != null) {
-			id = (String) ((HttpServletRequest) request).getSession().getAttribute("id");
-		}
+   /**
+    * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+    */
+   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+         throws IOException, ServletException {
+      String id = null;
+      
+      String gotoPage="";
+      
+      if(request.getParameter("gotoPage")==null||request.getParameter("gotoPage").equals("")) {
+      }else {
+         gotoPage=request.getParameter("gotoPage");
+      }
+      if (((HttpServletRequest) request).getSession().getAttribute("id") != null) {
+         id = (String) ((HttpServletRequest) request).getSession().getAttribute("id");
+      }
 
-		if (id == null) {
-			(((HttpServletRequest) request).getSession()).setAttribute("needToLogin", "plz");
-			(((HttpServletRequest) request).getSession()).setAttribute("needLoginMsg", "해당 서비스는 로그인 후 이용이 가능합니다.");
-			(((HttpServletRequest) request).getSession()).setAttribute("gotoPage", gotoPage);
-			((HttpServletResponse) response).sendRedirect("main.do");
-			
+      if (id == null) {
+         (((HttpServletRequest) request).getSession()).setAttribute("needToLogin", "plz");
+         (((HttpServletRequest) request).getSession()).setAttribute("needLoginMsg", "해당 서비스는 로그인 후 이용이 가능합니다.");
+         (((HttpServletRequest) request).getSession()).setAttribute("gotoPage", gotoPage);
+         ((HttpServletResponse) response).sendRedirect("main.do");
+         
 
-		} else {
-			chain.doFilter(request, response);
-		}
-	}
+      } else {
+         chain.doFilter(request, response);
+      }
+   }
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-	}
+   /**
+    * @see Filter#init(FilterConfig)
+    */
+   public void init(FilterConfig fConfig) throws ServletException {
+   }
 }
